bf7ef85462cfa24486718467f6bad8b9f7912fe2
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:57:53 +0530

    Program to Chech wether the number is Odd or Even

---
+++ oddeven.java
@@ -0,0 +1,16 @@
+//Program to Chech wether the number is Odd or Even
+class oddeven
+{
+    public static void main(String args[])
+    {
+        int a=3;
+        if(a%2==0)
+        {
+            System.out.println(a+"is an even number");
+        }
+        else
+        {
+            System.out.println(a+"is an odd number");
+        }
+    }
+}

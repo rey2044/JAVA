f5b8e2dd8f397fb22df4258cb70d20edb3c6b5a5
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:45:52 +0530

    java Program to Print Name Mid-name and Surname

---
+++ cmd2.java
@@ -0,0 +1,10 @@
+//java Program to Print Name Mid-name and Surname
+class cmd2
+{
+    public static void main(String args[])
+    {
+        System.out.println("First-Name:- "+args[0]);
+        System.out.println("Middle-Name:- "+args[1]);
+        System.out.println("Last-Name:- "+args[2]);
+    }
+}

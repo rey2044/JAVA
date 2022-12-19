abe636d808df93505c2326c605c8861919482ad5
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:58:18 +0530

    Java Program to print /the multiplication table of a given number

---
+++ table.java
@@ -0,0 +1,12 @@
+//Java Program to print /the multiplication table of a given number
+class table
+{
+    public static void main(String args[])
+    {
+        int no=7;
+        for(int i=0;i<=10;i++)
+        {
+            System.out.println(no+"*"+i+"="+no*i);
+        }
+    }
+}

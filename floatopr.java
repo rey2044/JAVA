e208d7b444ea3444d74e6d71b48d3640bbc96bd0
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:56:06 +0530

    add,sub,mul,div of floating numbers

---
+++ floatopr.java
@@ -0,0 +1,23 @@
+//add,sub,mul,div of floating numbers
+
+class floatopr
+{
+	public static void main(String args[])
+	{
+		float a = 9.5f;
+		float b = 5.4f;
+		float add,sub,mul,div;
+		add = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a / b;
+		System.out.println("addition = "+add);
+				
+		System.out.println("subtraction = "+sub);
+
+		System.out.println("multiplication = "+mul);
+
+		System.out.println("division = "+div);
+
+	}
+}

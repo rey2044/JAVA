8e0be12bf9ca1ed0c21fb85a1d97c0c5311d943b
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:42:42 +0530

    Java Program To check greater than or less than Numbers

---
+++ check.java
@@ -0,0 +1,21 @@
+//Java Program To check greater than or less than Numbers
+class check
+{
+    public static void main(String args[])
+    {
+    int a=7;
+    int b=9;
+    if(a>b)
+    {
+        System.out.println(a+"is greater than"+b);
+    }
+    else if(b>a)
+    {
+        System.out.println(b+"is greater than"+a);
+    }
+    else
+    {
+        System.out.println("Both numbers are equal");
+    }
+}
+}

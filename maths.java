156ea76149e2eace375e0691aaadf5eee4b6ad22
Rey Patel <63778144+rey2044@users.noreply.github.com>
2022-12-20 00:57:23 +0530

    add,sub,mul,div of two integer numbers

---
+++ maths.java
@@ -0,0 +1,23 @@
+//add,sub,mul,div of two numbers
+
+class maths
+{
+	public static void main(String args[])
+	{
+		int a = 10;
+		int b = 5;
+		int add,sub,mul,div;
+		add = a + b;
+		sub = a - b;
+		mul = a * b;
+		div = a / b;
+		System.out.println("addition = "+add);
+				
+		System.out.println("subtraction = "+sub);
+
+		System.out.println("multiplication = "+mul);
+
+		System.out.println("division = "+div);
+
+	}
+}
